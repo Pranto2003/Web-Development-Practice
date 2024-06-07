6b20708ab929c2ece45751cd1fe590e0f222f6f1
Pranto Goswamee <prantogoswamee21@gmail.com>
2024-06-07 17:13:06 +0600

    Basics of javascript

---
+++ Introduction_To_Javascript/script.js
@@ -0,0 +1,16 @@
+var a = 10;
+var b = 20;
+var c = "Pranto";
+console.log(a + b);
+console.log(typeof a, typeof b, typeof c);
+
+let o = {
+  "name ": "Pranto",
+  "Job Position": "CEO",
+  "Position Code": 4560,
+};
+
+console.log(o);
+o.salary = "500 Crores";
+console.log("After Adding Salary :");
+console.log(o);
