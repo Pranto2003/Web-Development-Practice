0d2b377ced5334dd28e460af422fd0c76d22a53f
Pranto Goswamee <prantogoswamee21@gmail.com>
2024-06-09 15:00:57 +0600

    A unique name generator for business

---
+++ UniqueBusinessNameGenerator/index.js
@@ -0,0 +1,23 @@
+let obj1 = {
+  1: "Crazy",
+  2: "Amazing",
+  3: "Fire",
+};
+
+let obj2 = {
+  1: "Engine",
+  2: "Foods",
+  3: "Garments",
+};
+
+let obj3 = {
+  1: "Bros",
+  2: "Limited",
+  3: "Hub",
+};
+
+let value1 = Math.floor(Math.random() * 3) + 1;
+let value2 = Math.floor(Math.random() * 3) + 1;
+let value3 = Math.floor(Math.random() * 3) + 1;
+
+console.log("---------------> "+obj1[value1] + " " + obj2[value2] + " " + obj3[value3]+" <---------------");
