1be3c2aa7760027cf790c08c20f15b40683fcb4d
pranto <prantogoswamee21@gmail.com>
2024-07-31 05:01:16 +0600

    Dom Manupulation

--- BasicsOfJavascript/DOM/Practice-2/script.js
+++ BasicsOfJavascript/DOM/Practice-2/script.js
@@ -1,6 +1,8 @@
 const boxes = document.querySelectorAll(".box");
 
 boxes[2].style.backgroundColor = "Yellow";
+boxes[2].style.color="Black"
+
 
 const newBox = document.querySelector("#redbox");
 newBox.style.backgroundColor = "red";
@@ -8,4 +10,12 @@ newBox.style.backgroundColor = "red";
 // boxes.forEach((e) => {
 //     e.style.backgroundColor = "green";
 //   });
-  
+
+boxes[1].style.backgroundColor="Black"
+boxes[1].style.color="White"
+
+boxes[3].style.backgroundColor="Purple"
+boxes[3].style.color="White"
+
+boxes[4].style.backgroundColor="Blue"
+boxes[4].style.color="White"
