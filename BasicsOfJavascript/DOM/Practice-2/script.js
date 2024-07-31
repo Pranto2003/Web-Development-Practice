622f93040fa93dc6d7b48306a6e718205d6df6d8
pranto <prantogoswamee21@gmail.com>
2024-07-31 10:08:55 +0600

    Updated Dom manipulation

--- BasicsOfJavascript/DOM/Practice-2/script.js
+++ BasicsOfJavascript/DOM/Practice-2/script.js
@@ -1,8 +1,7 @@
 const boxes = document.querySelectorAll(".box");
 
 boxes[2].style.backgroundColor = "Yellow";
-boxes[2].style.color="Black"
-
+boxes[2].style.color = "Black";
 
 const newBox = document.querySelector("#redbox");
 newBox.style.backgroundColor = "red";
@@ -11,11 +10,27 @@ newBox.style.backgroundColor = "red";
 //     e.style.backgroundColor = "green";
 //   });
 
-boxes[1].style.backgroundColor="Black"
-boxes[1].style.color="White"
+boxes[1].style.backgroundColor = "Black";
+boxes[1].style.color = "White";
 
-boxes[3].style.backgroundColor="Purple"
-boxes[3].style.color="White"
+boxes[3].style.backgroundColor = "Purple";
+boxes[3].style.color = "White";
 
-boxes[4].style.backgroundColor="Blue"
-boxes[4].style.color="White"
+boxes[4].style.backgroundColor = "Blue";
+boxes[4].style.color = "White";
+
+function getRandomColor() {
+  let var1 = Math.ceil(1 + Math.random() * 255);
+  let var2 = Math.ceil(1 + Math.random() * 355);
+  let var3 = Math.ceil(1 + Math.random() * 455);
+
+  return `rgb(${var1},${var2},${var3})`;
+}
+
+boxes.forEach((e) => {
+  e.style.backgroundColor = getRandomColor();
+  e.style.color = getRandomColor();
+  e.style.fontWeight = "Bolder";
+  e.style.fontSize = "19px";
+  e.style.padding = "5px";
+});
---
+++ BasicsOfJavascript/DOM/Practice-2/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+boxes[1].style.backgroundColor="Black"
+// boxes[1].style.color="White"
+
+// boxes[3].style.backgroundColor="Purple"
+// boxes[3].style.color="White"
+
+// boxes[4].style.backgroundColor="Blue"
+// boxes[4].style.color="White"
